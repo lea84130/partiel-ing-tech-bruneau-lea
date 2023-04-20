85d9e24bb899fcae90be575c9ed3b717214dfbae
Lea Bruneau <blea84130@gmail.com>
2023-04-20 09:07:04 +0200

    j'ai fait l'exo 4

--- exo-x/app.js
+++ exo-x/app.js
@@ -93,10 +93,83 @@ avion1.nbPlace = 853;
 avion1.reservoir = 320000;
 avion1.vitesse = 913;
 
-
 console.log(avion1.marque);
 console.log(avion1.modele);
 console.log(avion1.longueur);
 console.log(avion1.nbPlace); 
 console.log(avion1.reservoir); 
 console.log(avion1.vitesse);
+
+
+// exo 3
+
+class Avion {
+  constructor(marque, modele, longueur, nbPlace, reservoir, vitesse) {
+    this._marque = marque;
+    this._modele = modele;
+    this._longueur = longueur;
+    this._nbPlace = nbPlace;
+    this._reservoir = reservoir;
+    this._vitesse = vitesse;
+  }
+
+  get marque() {
+    return this._marque;
+  }
+}
+
+const avions = [];
+
+const avion1 = new Avion("Boeing", "747", 70, 416, 241140, 907);
+const avion2 = new Avion("Airbus", "A380", 73, 853, 320000, 913);
+const avion3 = new Avion("Boeing", "777", 63, 368, 181280, 905);
+const avion4 = new Avion("Airbus", "A320", 37, 150, 23600, 828);
+const avion5 = new Avion("Embraer", "E190", 36, 100, 16020, 834);
+
+avions.push(avion1, avion2, avion3, avion4, avion5);
+
+for (let i = 0; i < avions.length; i++) {
+  console.log("Avion " + (i + 1) + " : " + avions[i].marque);
+}
+
+
+// exo 4
+
+class Avion {
+  constructor(marque, modele, longueur, nbPlace, reservoir, vitesse) {
+    this._marque = marque;
+    this._modele = modele;
+    this._longueur = longueur;
+    this._nbPlace = nbPlace;
+    this._reservoir = reservoir;
+    this._vitesse = vitesse;
+  }
+
+  get nbPlace() {
+    return this._nbPlace;
+  }
+}
+
+const avions = [];
+
+const avion1 = new Avion("Boeing", "747", 70, 416, 241140, 907);
+const avion2 = new Avion("Airbus", "A380", 73, 853, 320000, 913);
+const avion3 = new Avion("Boeing", "777", 63, 368, 181280, 905);
+const avion4 = new Avion("Airbus", "A320", 37, 150, 23600, 828);
+const avion5 = new Avion("Embraer", "E190", 36, 100, 16020, 834);
+
+avions.push(avion1, avion2, avion3, avion4, avion5);
+
+function additionnerNombrePlaces(avions) {
+  let total = 0;
+  for (let i = 0; i < avions.length; i++) {
+    total += avions[i].nbPlace;
+  }
+  return total;
+}
+
+const totalNombrePlaces = additionnerNombrePlaces(avions);
+console.log("Le nombre total de places dans les avions est : " + totalNombrePlaces);
+
+
+
